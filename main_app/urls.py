bb7d1887fcea5aa50c3fe920b4146aa916af2c3e
LC Clark <laura.mc.clark@gmail>
2022-07-18 13:25:19 -0400

    adds chat index

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('/chats/', views.chats_index, name="index")
 ]
