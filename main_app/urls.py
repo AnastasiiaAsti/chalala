5c414be6cf45c9f707f0a61a40680760fd7b60ca
Asti <astistateofmind@gmail.com>
2022-07-18 12:05:17 -0700

    add chats update edit and detail func

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('accounts/signup/', views.signup, name='signup'),
     path('chats/', views.chats_index, name="index"),
-    path('chats/<int:pk>/delete/', views.ChatDelete.as_view(), name='chats_delete')
+    path('chats/<int:pk>/delete/', views.ChatDelete.as_view(), name='chats_delete'),
+    path('chats/create/', views.ChatCreate.as_view(), name='chats_create'),
+    path('chats/<int:pk>/', views.ChatDetail.as_view(), name="chats_detail"),
+    path('chats/<int:pk>/update/', views.ChatUpdate.as_view(), name="chats_update"),
 ]
